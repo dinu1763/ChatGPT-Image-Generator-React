4c8fd2a10a5b401c78db7987d5b7c1773b0c985d
Dinesh <dinu1763@gmail.com>
2023-04-21 19:49:41 +0530

    remove api

--- chatgpt-image/src/App.jsx
+++ chatgpt-image/src/App.jsx
@@ -6,28 +6,43 @@ import { useState } from 'react';
 function App() {
   const [prompt, setPrompt] = useState('');
   const [result, setResult] = useState('');
+  const [loading, setLoading] = useState(false);
   const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: process.env.REACT_APP_API_KEY
 });
 const openai = new OpenAIApi(configuration);
 const generateImage = async () => {
+  setLoading(true);
    const res = await openai.createImage({
     prompt: prompt,
     n: 1,
     size: "1024x1024",
 });
+setLoading(false);
 setResult(res.data.data[0].url);
 }
 
   return (
     <div className="app-main">
-      <h1>Generate an Image using Artificial Intelligence </h1>
-      <input className='app-input' placeholder='Type here to Generate an AI Image...' type="text" onChange={(e) => {
-        e.preventDefault();
-        setPrompt(e.target.value);
-        }}/>
-      <button onClick={generateImage}>Generate an Image</button>
-      {result.length>0? <img className='result-image' src={result} alt="" /> : <></>}
+      {loading ? (
+         <>
+          <h2>Generating..Please Wait..</h2>
+          <div className="lds-ripple">
+            <div></div>
+            <div></div>
+          </div>
+        </>
+      ) : (
+        <>
+        <h1>Generate an Image using Artificial Intelligence </h1>
+        <input className='app-input' placeholder='Type here to Generate an AI Image...' type="text" onChange={(e) => {
+          e.preventDefault();
+          setPrompt(e.target.value);
+          }}/>
+        <button onClick={generateImage}>Generate an Image</button>
+        {result.length>0? <img className='result-image' src={result} alt="" /> : <></>}
+        </>
+      )}
     </div>
   );
 }
